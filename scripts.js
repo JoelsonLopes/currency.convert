c017baac8ca181c67a59edd0b7678424c8d2231f
JoelsonLopes <joelsonlopes85@gmail.com>
2023-06-13 19:33:16 -0300

    Javascript changes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,47 @@
-const convertButton = document.querySelector(".button-convert");
+const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
+
+function convertValues() {
+  const inputCurrencyValue = document.querySelector(".input-currency").value;
+  const currencyValueToConvert = document.querySelector(
+    ".currency-value-to-convert"
+  ); //Valor em Real
+  const currencyValueConverted = document.querySelector(".currency-value"); // Outras moedas
+
+  console.log(currencySelect.value);
+  const dolarToday = 5.2;
+  const euroToday = 6.2;
+  const libraToday = 5.8;
+
+  if (currencySelect.value == "dolar") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday);
+  }
+  if (currencySelect.value == "euro") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrencyValue / euroToday);
+  }
+  if (currencySelect.value == "libra") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP",
+    }).format(inputCurrencyValue / dolarToday);
+  }
+
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrencyValue);
+}
+
+convertButton.addEventListener("click", convertValues);
+
+/*const convertButton = document.querySelector(".button-convert");
+const selectMainTo = document.querySelector(".select-main-to");
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
@@ -7,17 +50,54 @@ function convertValues() {
   ); // VALOR EM REAIS
   const currencyValueConvert = document.querySelector(".currency-value"); // OUTRAS MOEDAS
 
-  const dolarToday = 5.2;
-  const convertedValue = inputCurrencyValue / dolarToday;
+  const selectMain = document.querySelector(".select-main");
 
+  const dolarToday = 5.2;
+  const euroToday = 6.2;
+  const libraToday = 6.12;
+
+  if (selectMainTo.value == "Dolar") {
+    //Se o select estiver  selecionado o valor dolar, entra aqui
+    currencyValueConvert.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday); // função para formatar valores em en-US
+  }
+  if (selectMainTo.value == "Euro") {
+    //Se o select estiver  selecionado o valor euro, entra aqui
+    currencyValueConvert.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrencyValue / euroToday); // função para formatar valores em de-DE
+  }
+  if (selectMainTo.value == "Libra") {
+    //Se o select estiver  selecionado o valor libra, entra aqui
+    currencyValueConvert.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "GBP",
+    }).format(inputCurrencyValue / libraToday); // função para formatar valores em de-DE
+  }
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-  }).format(inputCurrencyValue);
-
-  currencyValueConvert.innerHTML = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(convertedValue);
+  }).format(inputCurrencyValue); // função para formatar valores em pt-br
 }
-convertButton.addEventListener("click", convertValues);
+
+function changeSelectMainTo() {
+  const selectMainTo = document.getElementsByClassName(".currency-name");
+
+  if (selectMainTo.value == "Dolar") {
+    currencyName.innerHTML = "Dólar americano";
+  }
+  if (selectMainTo.value == "Euro") {
+    currencyName.innerHTML = "Euro";
+  }
+  if (selectMainTo.value == "Libra") {
+    currencyName.innerHTML = "Libra";
+  }
+
+  convertValues();
+}
+
+selectMainTo.addEventListener("change", changeSelectMainTo); //Função para saber quando a moeda for trocada no select
+convertButton.addEventListener("click", convertValues); //Função para saber quando o button de converter foi clicado */
